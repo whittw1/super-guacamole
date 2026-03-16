23d3aba8cbbec6bf4383dbe0f2d6cfa010854aa2
whittw1 <wilkins@hgsengineeringinc.com>
2026-03-16 05:48:27 +0000

    Add files via upload

---
+++ sw_notes.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'dla-notes-v1.0';
+const URLS_TO_CACHE = [
+  './',
+  './notes.html',
+  'https://unpkg.com/docx@8.2.2/build/index.umd.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
